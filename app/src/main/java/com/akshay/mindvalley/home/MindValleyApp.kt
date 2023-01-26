f53f001017b30b0a4b446714e8b2455a59a60100
akshay <akshaytrivedi92@gmail.com>
2023-01-26 20:14:11 +0530

    Api level 19 app crash fix

--- app/src/main/java/com/akshay/mindvalley/home/MindValleyApp.kt
+++ app/src/main/java/com/akshay/mindvalley/home/MindValleyApp.kt
@@ -1,7 +1,7 @@
 package com.akshay.mindvalley
 
-import android.app.Application
+import androidx.multidex.MultiDexApplication
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MindValleyApp : Application()
+class MindValleyApp : MultiDexApplication()
